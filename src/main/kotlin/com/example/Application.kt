b65584962f58e4cc60f8cf1217ddd9e7b1c2fb71
Dhaval Shah <dhaval@alphastack.in>
2022-04-03 06:45:34 +0530

    Adding local file to store data

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,6 +2,7 @@ package com.example
 
 import com.example.api.users.configureUsersRouting
 import com.example.data.db.UsersDataSource
+import com.google.gson.Gson
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.application.*
@@ -9,7 +10,9 @@ import io.ktor.features.*
 import io.ktor.gson.*
 
 fun main() {
-    val usersDataSource = UsersDataSource()
+    val gson = Gson()
+    val filePath = "./data/users.text"
+    val usersDataSource = UsersDataSource(gson, filePath)
 
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureUsersRouting(usersRepository = usersDataSource)
--- src/main/kotlin/com/example/data/db/UsersDataSource.kt
+++ src/main/kotlin/com/example/data/db/UsersDataSource.kt
@@ -2,17 +2,53 @@ package com.example.data.db
 
 import com.example.domain.users.model.User
 import com.example.domain.users.repositories.UsersRepository
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileWriter
+import java.io.PrintWriter
 import java.util.concurrent.atomic.AtomicLong
 
-class UsersDataSource : UsersRepository {
+class UsersDataSource(
+    private val gson: Gson,
+    private val filePath: String
+) : UsersRepository {
 
-    private val idCounter = AtomicLong(0)
+    private val idCounter: AtomicLong
     private val users = mutableListOf<User>()
 
+    init {
+        val text = File(filePath).readText()
+        if (text.isNotBlank()) {
+            val typeToken = object : TypeToken<List<User>>(){}.type
+            val loadedUsers: List<User> = gson.fromJson(text, typeToken)
+            users.clear()
+            users.addAll(loadedUsers)
+
+            idCounter = AtomicLong(users.last().id)
+        } else {
+            idCounter = AtomicLong(0)
+        }
+    }
+
     override suspend fun addUser(user: User): User {
-        val userToInsert = user.copy(id = idCounter.incrementAndGet())
-        users.add(userToInsert)
-        return userToInsert
+        return withContext(Dispatchers.IO) {
+            val userToInsert = user.copy(id = idCounter.incrementAndGet())
+            users.add(userToInsert)
+
+            try {
+                PrintWriter(FileWriter(filePath)).use {
+                    val jsonData = gson.toJson(users)
+                    it.write(jsonData)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            userToInsert
+        }
     }
 
     override suspend fun getUsers(): List<User> {
